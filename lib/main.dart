ec2cbc6403a5490fe91486aba87dae67fbf77757
wakino <twakino@kuraseru.jp>
2019-09-17 10:40:33 +0900

    簡易モック

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_app/pages/login.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'Navigation Basics',
+    home: new LoginPage(),
+  ));
+}
+
